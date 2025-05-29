af61f796478ec9c0a2cc31cada89a35139d74694
Hayden Steele <haydenallensteene@gmail.com>
2025-05-29 11:15:07 -0600

    Added role indicator to header

--- src/components/User/InteractiveUserIcon.tsx
+++ src/components/User/InteractiveUserIcon.tsx
@@ -1,18 +1,25 @@
 import { getUserFromSession } from "@/auth/auth"
+import getOrgRole from "@/auth/roles/getOrgRole"
 import Link from "next/link"
 
 
 export default async function InteractiveUserIcon() {
 
     const user = await getUserFromSession()
+    const role = await getOrgRole()
 
     if (!user) {
         return (<p>Error...</p>)
     }
 
     return (
-        <div className="mt-1">
-            <Link href={"/user"} className="font-black text-xl">
+        <div className="mt-1 flex flex-row">
+
+            <div className="p-1 rounded-md text-white font-bold mr-2 max-h-8" style={{backgroundColor: role.color}}>
+                {role.type}
+            </div>
+
+            <Link href={"/user"} className="font-black text-xl ">
                 {user.firstName + " " + user.lastName}
             </Link>
         </div>
