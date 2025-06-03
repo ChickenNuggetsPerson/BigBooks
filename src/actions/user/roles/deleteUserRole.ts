1363bca06bdc0857a1a632f6c2ec18c3c88e8b86
Hayden Steele <haydenallensteene@gmail.com>
2025-06-03 02:32:28 -0600

    fix: Fixed permissions with admins and editing other admin roles

--- src/actions/user/roles/deleteUserRole.ts
+++ src/actions/user/roles/deleteUserRole.ts
@@ -1,7 +1,8 @@
 'use server'
 
 import { redirectIfInvalidSession } from "@/auth/auth"
-import { RoleTypes } from "@/auth/roles/Roles"
+import getOrgRole from "@/auth/roles/getOrgRole"
+import { getRoleFromID, RoleTypes } from "@/auth/roles/Roles"
 import { throwIfInsufficientPerms } from "@/auth/roles/throwIfInsufficientPerms"
 import { prisma } from "@/database/prisma"
 import { revalidatePath } from "next/cache"
@@ -16,7 +17,13 @@ export default async function deleteUserRole(userUUID: string, orgUUID: string) 
     await redirectIfInvalidSession()
     await throwIfInsufficientPerms(RoleTypes.Admin)
 
-    await prisma.role.deleteMany({ where: { userId: userUUID, organizationId: orgUUID }})
+    const currentRole = await prisma.role.findFirst({ where: { userId: userUUID, organizationId: orgUUID } })
+    const userRole = await getOrgRole()
+    if (userRole.level <= getRoleFromID(currentRole?.role ?? "").level) { // You cannot edit the roles of other admins 
+        throw new Error("Insufficient Permissions")
+    }
+
+    await prisma.role.deleteMany({ where: { userId: userUUID, organizationId: orgUUID } })
 
     revalidatePath("/user/users/" + userUUID)
 }
--- src/actions/user/roles/editUserRole.ts
+++ src/actions/user/roles/editUserRole.ts
@@ -1,7 +1,8 @@
 'use server'
 
 import { redirectIfInvalidSession } from "@/auth/auth"
-import { getIDFromRoleType, RoleTypes } from "@/auth/roles/Roles"
+import getOrgRole from "@/auth/roles/getOrgRole"
+import { getIDFromRoleType, getRoleFromID, RoleTypes } from "@/auth/roles/Roles"
 import { throwIfInsufficientPerms } from "@/auth/roles/throwIfInsufficientPerms"
 import { prisma } from "@/database/prisma"
 import { revalidatePath } from "next/cache"
@@ -18,6 +19,12 @@ export default async function editUserRole(userUUID: string, orgUUID: string, ro
     const r = await prisma.role.findFirst({ where: { userId: userUUID, organizationId: orgUUID } })
     if (!r) {
         throw new Error("Role does not exist")
+    }
+
+    const currentRole = await prisma.role.findFirst({ where: { userId: userUUID, organizationId: orgUUID }})
+    const userRole = await getOrgRole()
+    if (userRole.level <= getRoleFromID(currentRole?.role ?? "").level) { // You cannot edit the roles of other admins 
+        throw new Error("Insufficient Permissions")
     }
 
     await prisma.role.update({
