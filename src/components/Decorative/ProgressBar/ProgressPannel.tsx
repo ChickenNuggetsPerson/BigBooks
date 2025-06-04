687978d1d11c918b0f722e3438101e4c80dc4ae9
Hayden Steele <haydenallensteene@gmail.com>
2025-06-04 00:30:40 -0600

    fix: Color and speed changes to progress bar.

--- src/components/Decorative/ProgressBar/ProgressPannel.tsx
+++ src/components/Decorative/ProgressBar/ProgressPannel.tsx
@@ -20,7 +20,7 @@ export default function ProgressBar({ steps, currentStep, changeCB }: ProgressBa
         return `0 ${size * (steps.length - (currentStep + 1))}% 0 ${currentStep * size}%`
     }
 
-    const transition = "clip-path .2s ease-out"
+    const transition = "clip-path .3s cubic-bezier(0.25, 0.1, 0.25, 1)"
 
     return (
         <div className="flex flex-row justify-center">
@@ -28,14 +28,14 @@ export default function ProgressBar({ steps, currentStep, changeCB }: ProgressBa
                 {/* Background Layer */}
 
                 <div
-                    className="absolute inset-0 bg-primary z-10"
+                    className="absolute inset-0 bg-accent z-10"
                     style={{
                         transition: transition,
                         clipPath: `inset(${getSelectedPercent()})` // you can dynamically apply your clipPath here
                     }}
                 ></div>
                 <div
-                    className="absolute inset-0 bg-primary opacity-30 z-0"
+                    className="absolute inset-0 bg-primary opacity-20 z-0"
                     style={{
                         transition: transition,
                         clipPath: `inset(0 ${getPercent()} 0 0)` // you can dynamically apply your clipPath here
@@ -48,8 +48,11 @@ export default function ProgressBar({ steps, currentStep, changeCB }: ProgressBa
                 {steps.map((step, index) => (
                     <div
                         key={step}
-                        className="relative z-10 flex-1 px-4 py-3 text-center text-sm font-medium transition-colors duration-200 border-r border-secondary last:border-r-0"
+                        className="select-none text-gray-600 relative z-10 flex-1 px-4 py-3 text-center text-sm font-bold transition-colors duration-200 border-r border-secondary last:border-r-0"
                         onClick={() => changeCB(index)}
+                        style={{
+                            color: currentStep == index ? "white" : ""
+                        }}
                     >
                         {step}
                     </div>
