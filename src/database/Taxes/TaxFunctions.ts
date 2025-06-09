d78f41d43dd7f28b93205932e453c6c57ef23b99
Hayden Steele <haydenallensteene@gmail.com>
2025-06-08 23:52:19 -0600

    added comments

--- src/database/Taxes/TaxFunctions.ts
+++ src/database/Taxes/TaxFunctions.ts
@@ -7,6 +7,8 @@ import { Employee, PayStubItem } from "../generated/prisma";
 
 
 // TODO: Link this to the employee some how. So then totals can be seen for the different tax types
+// Make a new model for storing taxes I guess...
+// Store tax types as enums as well
 export function calcEmployeeTaxes(employee: Employee) : PayStubItem[] {
     console.log(employee)
     return []
