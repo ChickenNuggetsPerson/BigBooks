b2f2ec76807cd1e44b3272d4892eeac64e7343af
Hayden Steele <haydenallensteele@gmail.com>
2025-12-24 17:11:11 -0700

    Removed weird edge to admin card

--- src/components/User/UserMembershipIcon.tsx
+++ src/components/User/UserMembershipIcon.tsx
@@ -16,10 +16,13 @@ export function UserMembershipIcon({ membership }: { membership: Membership }) {
         <ClickableDiv
             className="py-1 text-white font-bold select-none h-fit smallCard"
             style={{
-                background: membership.orgAdmin ? "linear-gradient(90deg,rgba(162, 98, 204, 1) 1%, rgba(227, 91, 91, 1) 48%, rgba(255, 194, 110, 1) 100%)": "#102030",
+                background: membership.orgAdmin 
+                ? "linear-gradient(90deg,rgba(162, 98, 204, 1) 1%, rgba(227, 91, 91, 1) 48%, rgba(255, 194, 110, 1) 100%)"
+                : "#102030",
                 borderRadius: 10,
                 paddingRight: 10,
-                paddingLeft: 10
+                paddingLeft: 10,
+                border: "none"
             }}
         >
             {membership.orgAdmin && <p>Admin</p>}
