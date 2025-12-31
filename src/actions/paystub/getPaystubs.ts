96b16f9a98765f4941c0168e3c9a01bc32ea2fe3
Hayden Steele <haydenallensteele@gmail.com>
2025-12-31 12:32:50 -0700

    Added getPaystubs action

---
+++ src/actions/paystub/getPaystubs.ts
@@ -0,0 +1,28 @@
+"use server"
+
+import { throwIfInsufficientPerms } from "@/auth/permissions/PermissionsFunctions";
+import { PaystubFilters } from "./paystubFilters";
+import { Permissions } from "@/auth/permissions/PermissionsDef";
+import { prisma } from "@/database/prisma";
+import { getSession } from "@/auth/auth";
+import { serializeData } from "@/utils/serialization";
+
+
+export default async function getPaystubs(identifiers: PaystubFilters) {
+
+    const session = await getSession()
+    await throwIfInsufficientPerms(Permissions.payroll.paystub.view)
+
+    const paystubs = await prisma.payStub.findMany({
+        where: {
+            ...identifiers,
+            ...{
+                employee: {
+                    organizationId: session?.orgUUID ?? ""
+                }
+            }
+        }
+    })
+
+    return serializeData(paystubs)
+}
