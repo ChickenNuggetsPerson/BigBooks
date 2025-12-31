50a96bde6a7037afc7d285111f936449fc0430ba
Hayden Steele <haydenallensteele@gmail.com>
2025-12-31 11:54:01 -0700

    Updated Stats card to have paystub total

--- src/components/Organization/OverviewPage/OrganizationStatsCard.tsx
+++ src/components/Organization/OverviewPage/OrganizationStatsCard.tsx
@@ -1,9 +1,13 @@
+import getPaystubTotals from "@/actions/paystub/paystubStats/getPaystubTotals"
 import { getSession } from "@/auth/auth"
 import CollapsibleDiv from "@/components/Decorative/CollapsibleDiv"
 import LoadingBlock from "@/components/Decorative/LoadingBlock"
 import { CardProp } from "@/components/Forms/CardProp"
 import { Divider } from "@/components/Forms/Divider"
+import { Decimal } from "@/database/generated/prisma/runtime/library"
 import { prisma } from "@/database/prisma"
+import { MoneyToStr } from "@/utils/functions/MoneyStr"
+import { deserializeData } from "@/utils/serialization"
 
 
 
@@ -13,6 +17,7 @@ export function OrganizationStatsCard_Loading() {
         <div className="card h-fit">
             <LoadingBlock w={"full"} />
             <Divider />
+            <LoadingBlock w={"full"} className="mb-2" />
             <LoadingBlock w={"full"} className="mb-2" />
             <LoadingBlock w={"full"} className="mb-2" />
             <LoadingBlock w={"full"} className="mb-2" />
@@ -26,19 +31,25 @@ export default async function OrganizationStatsCard() {
     const employeeCount = await prisma.employee.count({ where: { organizationId: session?.orgUUID } })
     const completedPaystubCount = await prisma.payStub.count({ where: { employee: { organizationId: session?.orgUUID }, locked: true } })
     const activePaystubCount = await prisma.payStub.count({ where: { employee: { organizationId: session?.orgUUID }, locked: false } })
+    const orgTotals = (new Decimal(deserializeData(await getPaystubTotals({})).totalNetPay)).toNumber()
 
     return (
         <div className="card">
             <h5 className="mb-2 text-2xl font-normal text-gray-700 ">Organization Statistics:</h5>
             <Divider />
             <CardProp label="Total Employees:" val={employeeCount} />
-            
-            <CollapsibleDiv title={<CardProp label="Total Paystubs:" val={completedPaystubCount + activePaystubCount}/>} arrowSize={16} >
-                <div className="flex flex-row justify-between px-2">
-                    <CardProp label="Completed:" val={completedPaystubCount} />
-                    <CardProp label="Active:" val={activePaystubCount} />
-                </div>
-            </CollapsibleDiv>
+
+            <div className="bg-gray-300 w-full rounded-2xl h-px"></div>
+
+            <CardProp label="Total Paystubs:" val={completedPaystubCount + activePaystubCount} />
+            <div className="flex flex-row justify-between px-2">
+                <CardProp label="Completed:" val={completedPaystubCount} />
+                <CardProp label="Active:" val={activePaystubCount} />
+            </div>
+
+            <div className="bg-gray-300 w-full rounded-2xl h-px"></div>
+
+            <CardProp label="Total Net Pay:" val={MoneyToStr(orgTotals)} />
 
         </div>
     )
